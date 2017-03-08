83cfc964ee7f79dfd2f2531e9957712998570294
Robert Mueller <romueller@techfak.uni-bielefeld.de>
2017-03-08 15:54:04 +0100

    Adds licence information and updates year information in the copyright line.

--- src/Preprocessor.cpp
+++ src/Preprocessor.cpp
@@ -1,9 +1,20 @@
 /*
  * SCT-PJ
  *
- * Copyright (C) 2016 Robert Mueller
+ * Copyright (C) 2016 - 2017 Robert Mueller
  *
- * TODO add licence text (e.g. GNU (Affero) General Public License)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Contact: Robert Mueller <romueller@techfak.uni-bielefeld.de>
  * Faculty of Technology, Bielefeld University,
